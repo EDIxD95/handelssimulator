3585d912691b9a76fee36ce03749e3ad322c3dce
antcedric91 <ant.cedric91@gmail.com>
2024-06-20 11:41:19 +0200

    Modell: Lager vorbereitet

--- Handelssimulator/src/model/Lager.java
+++ Handelssimulator/src/model/Lager.java
@@ -1,5 +1,27 @@
 package model;
 
-public class Lager {
+import java.util.ArrayList;
 
+public class Lager {
+	private int maxKapazitaet;
+	private int gold;
+	
+	//ArrayList<Produkt> Lagerbestand = new ArrayList<Produkt>();
+
+	public Lager() {}
+	
+	public int getMaxKapazitaet() {
+		return maxKapazitaet;
+	}
+	public void setMaxKapazitaet(int maxKapazitaet) {
+		this.maxKapazitaet = maxKapazitaet;
+	}
+	public int getGold() {
+		return gold;
+	}
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+
+	
 }
