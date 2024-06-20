b1c3de79d44b01d75412c7f289a8ada83a7633f3
Velsabor <rfnew22@outlook.de>
2024-06-20 12:04:37 +0200

    Markt

--- Handelssimulator/src/model/Marktplatz.java
+++ Handelssimulator/src/model/Marktplatz.java
@@ -1,5 +1,13 @@
 package model;
 
-public class Marktplatz {
+import java.util.ArrayList;
 
-}
+public class Marktplatz {
+    private static <String, Integer> lager();
+
+    public static void main(String[] args) {
+        lager.put("Gold", 100);
+        lager.put("Eisen", 200);
+        lager.put("Nahrung", 300);
+
+     
